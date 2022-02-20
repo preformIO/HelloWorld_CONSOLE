de538133724f3d6f7e5dac5d164f3a014db2e909
david allen <allend5@rpi.edu>
2022-02-20 10:44:04 -0500

    Force appliction to stay running until user presses enter key.

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -2,10 +2,17 @@
 //
 
 #include <iostream>
+#include <limits>
 
 int main()
 {
     std::cout << "Hello World!\n";
+
+    // force appliction to stay running until user presses enter key
+    // ---------------------------------------------------------------
+    std::cin.clear(); // reset any error flags
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // ignore any characters in the input buffer until we find an enter character
+    std::cin.get(); // get one more char from the user
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
