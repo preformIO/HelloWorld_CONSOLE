806902033239fc41890120fe1c6fea2d1d5242e4
david allen <allend5@rpi.edu>
2022-02-26 21:14:21 -0500

    Turned warning levels up to maximum

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,4 +1,16 @@
-// HelloWorld.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// HelloWorld.cpp : This file contains the 'main' function. 
+// Program execution begins and ends there.
+// 
+// History:
+// 2022.02.26
+//  - turned warning levels up to the maximum
+//  https://www.learncpp.com/cpp-tutorial/configuring-your-compiler-warning-and-error-levels/
+//  - disabled language extensions
+//  https://www.learncpp.com/cpp-tutorial/configuring-your-compiler-compiler-extensions/     
+// 2022.02.20
+//  - Initial setup 
+//  https://www.learncpp.com/cpp-tutorial/compiling-your-first-program/
+// 
 //
 
 #include <iostream>
@@ -11,7 +23,9 @@ int main()
     // force appliction to stay running until user presses enter key
     // ---------------------------------------------------------------
     std::cin.clear(); // reset any error flags
-    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // ignore any characters in the input buffer until we find an enter character
+    // ignore any characters in the input buffer until we find an 
+    // enter character
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); 
     std::cin.get(); // get one more char from the user
 }
 
@@ -23,5 +37,9 @@ int main()
 //   2. Use the Team Explorer window to connect to source control
 //   3. Use the Output window to see build output and other messages
 //   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+//   5. Go to Project > Add New Item to create new code files, 
+//      or Project > Add Existing Item to add existing code files 
+//      to the project
+//   6. In the future, to open this project again, go to 
+//        File > Open > Project and select the .sln file
+//
